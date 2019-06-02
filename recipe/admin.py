1efe6669e37027afb84962729ab4b59359df05fc
Joel Tang <dangjoeltang@gmail.com>
2019-06-02 17:35:57 -0500

    created admin registrations

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Ingredient, Recipe, RecipeIngredients
+
+
 # Register your models here.
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredients
+    can_delete = False
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    # list_display = ('title', 'author', 'ingredients')
+    ordering = ('title', 'author')
+    search_fields = ('title', 'author', 'ingredients')
+    inlines = (RecipeIngredientInline,)
+
+admin.site.register(Ingredient)
+
--- recipe/models.py
+++ recipe/models.py
@@ -5,18 +5,52 @@ from django.db import models
 from django.utils import timezone
 
 
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Ingredient Name',
+        max_length=64
+    )
+    description = models.TextField(
+        verbose_name='Ingredient Description'
+    )
+
+
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default="")
+    title = models.CharField(
+        verbose_name='Recipe Title',
+        max_length=200,
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='Recipe Author',
+        on_delete=models.CASCADE,
+        default=''
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredients'
+    )
 
     created_time = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
 
 class RecipeIngredients(models.Model):
-    pass
-
-
-class Ingredient(models.Model):
-
-    pass
+    ingredient = models.ForeignKey(
+        Ingredient,
+        verbose_name='Ingredient used',
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Recipe used',
+        on_delete=models.CASCADE
+    )
+    quantity_amount = models.CharField(
+        verbose_name='Ingredient quantity',
+        max_length=10
+    )
+    quantity_unit = models.CharField(
+        verbose_name='Ingredient quantity units',
+        max_length=24
+    )
