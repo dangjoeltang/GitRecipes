ff449b555729c765bdc1a27bdaa29fabac4fb7dc
Joel Tang <dangjoeltang@gmail.com>
2019-06-24 01:23:41 -0500

    untracked views

---
+++ recipe/views.py.5aa95754255479ebf93f0ef1f44f5a5e.py
@@ -0,0 +1,135 @@
+from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework import mixins
+
+from .models import *
+from .serializers import *
+
+
+class IngredientViewset(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientDetailSerializer
+
+
+class TagViewset(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class RecipeListView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeListSerializer
+
+
+class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeTagListView(generics.ListCreateAPIView):
+    queryset = RecipeTag.objects.all()
+    serializer_class = RecipeTagSerializer
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeTag.objects.filter(recipe__pk=recipe_pk)
+
+    def perform_create(self, serializer):
+        recipe = Recipe.objects.get(id=self.kwargs['pk'])
+        serializer.save(recipe=recipe)
+
+
+class RecipeTagDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RecipeTag.objects.all()
+    serializer_class = RecipeTagSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeTag.objects.filter(recipe__pk=recipe_pk)
+
+
+class RecipeIngredientListView(generics.ListCreateAPIView):
+    queryset = RecipeIngredient.objects.all()
+    serializer_class = RecipeIngredientSetSerializer
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeIngredient.objects.filter(recipe__pk=recipe_pk)
+
+    def perform_create(self, serializer):
+        recipe = Recipe.objects.get(id=self.kwargs['pk'])
+        serializer.save(recipe=recipe)
+
+
+class RecipeIngredientDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RecipeIngredient.objects.all()
+    serializer_class = RecipeIngredientSetSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        recipe_ingredient_pk = self.kwargs['id']
+
+        return RecipeIngredient.objects.filter(recipe__pk=recipe_pk)
+
+
+class RecipeStepListView(generics.ListCreateAPIView):
+    queryset = RecipeStep.objects.all()
+    serializer_class = RecipeStepSerializer
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeStep.objects.filter(recipe__pk=recipe_pk)
+
+    def perform_create(self, serializer):
+        recipe = Recipe.objects.get(id=self.kwargs['pk'])
+        serializer.save(recipe=recipe)
+
+
+class RecipeStepDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RecipeStep.objects.all()
+    serializer_class = RecipeStepSerializer
+    lookup_field = 'step_number'
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeStep.objects.filter(recipe__pk=recipe_pk)
+
+
+class RecipeNoteListView(generics.ListCreateAPIView):
+    queryset = RecipeNote.objects.all()
+    serializer_class = RecipeNoteSerializer
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeNote.objects.filter(recipe__pk=recipe_pk)
+
+    def perform_create(self, serializer):
+        recipe = Recipe.objects.get(id=self.kwargs['pk'])
+        serializer.save(recipe=recipe)
+
+
+class RecipeNoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RecipeNote.objects.all()
+    serializer_class = RecipeNoteSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        recipe_pk = self.kwargs['pk']
+        return RecipeNote.objects.filter(recipe__pk=recipe_pk)
+
+
+class RecipeViewset(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = GenericRecipeSerializer
+
+    # def update(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     serializer = GenericRecipeSerializer(
+    #         instance=instance,
+    #         data=request.data
+    #     )
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(serializer.data)
