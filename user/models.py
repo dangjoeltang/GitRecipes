65a028f23ebd02de9b70c90bb3068c7d4c3785f4
Joel Tang <dangjoeltang@gmail.com>
2019-07-15 23:21:43 -0500

    push to heroku

--- user/models.py
+++ user/models.py
@@ -19,7 +19,7 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     short_bio = models.CharField(max_length=400, blank=True)
-    profile_photo = models.FileField(upload_to='profile_pics/', blank=True)
+    profile_photo = models.FileField(blank=True)
 
     created_time = models.DateTimeField(
         auto_now_add=True, blank=True, null=True)
