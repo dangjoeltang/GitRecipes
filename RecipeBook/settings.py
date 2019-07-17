5f850a11a88a416ca8f96236ed8f69f23b5f98b6
Joel Tang <dangjoeltang@gmail.com>
2019-07-16 21:06:16 -0500

    static setup changes

--- RecipeBook/settings.py
+++ RecipeBook/settings.py
@@ -176,10 +176,10 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'RecipeBook.storage_backends.MediaStorage'
 
 # Configure Django App for Heroku.
