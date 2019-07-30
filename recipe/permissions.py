1064223e36d2c849823e58eb44c366a7dab66ddd
Qiaowei Tang <qiaowei.tang@ad.infosys.com>
2019-07-30 16:57:48 -0500

    recipe list permissions

--- recipe/permissions.py
+++ recipe/permissions.py
@@ -2,6 +2,15 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'list':
+            return True
+        elif view.action == 'create':
+            return request.user.is_authenticated
+        elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+            return True
+        else:
+            return False
     """
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
