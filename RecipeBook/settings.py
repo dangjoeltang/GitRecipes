0dd6ef1da232479c5d65af8b445b25ebae21585a
Joel Tang <dangjoeltang@gmail.com>
2019-07-29 23:13:11 -0500

    add packages to requirements.txt

--- RecipeBook/settings.py
+++ RecipeBook/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
